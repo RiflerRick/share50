e590e26d32d6c7f53662582f5793460e63248ce0
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2016-09-25 00:13:07 +0530

    WIP: myTours page prepared just some situations to be tested

--- public/myTours.js
+++ public/myTours.js
@@ -1,89 +1,140 @@
 document.body.onload=function(){
-var url="checkMyTours";
 
+var url="checkMyTours";
 $.getJSON(url,function(data){
 
-  var invites=data["invites"];
-  var ownTours=data["ownTours"];
-  // alert("check");
-    var element,elementHeading,elementNode,elementBody,elementBodyNode,elementCheckBox;
-    var uid,name,email;
-    if(invites!=0)
-    {
-      element=document.createElement("div");
-      element.setAttribute("class","alert alert-info");
-      elementNode=document.createTextNode("Friends who wanted you to accompany them");
-      element.appendChild(elementNode);
-      document.getElementById("wellContent").appendChild(element);
-      for (var key in data)
+  window.setTimeout(function(){
+
+    var invites=data["invites"];
+    var ownTours=data["ownTours"];
+    // alert("check");
+      var element,elementHeading,elementNode,elementBody,elementBodyNode,elementCheckBox;
+      var uid,name,email;
+      if(invites!=0)
       {
-        // alert("data now is in process");
-        // alert("data is: "+data[key]);
-        if(key.substring(0,3).localeCompare("own")==0)
-        break;
-        if(key.substring(0,3).localeCompare("uid")==0)
-        uid=data[key];
-        if(key.substring(0,4).localeCompare("name")==0)
-        name=data[key];
-        if(key.substring(0,5).localeCompare("email")==0)
+        element=document.createElement("div");
+        element.setAttribute("class","alert alert-info");
+        elementNode=document.createTextNode("Friends who wanted you to accompany them");
+        element.appendChild(elementNode);
+        document.getElementById("wellContent").appendChild(element);
+        for (var key in data)
         {
-          email=data[key];
-          element=document.createElement("div");
-          element.setAttribute("class","panel panel-primary");
-          elementHeading=document.createElement("div");
-          elementHeading.setAttribute("class","panel-heading");
-          elementBody=document.createElement("div");
-          elementBody.setAttribute("class","panel-body");
-          elementNode=document.createTextNode(name);
-          elementBodyNode=document.createTextNode(email);
-          elementCheckBox=document.createElement("input");
-          elementCheckBox.setAttribute("type","checkbox");
-          elementCheckBox.style.marginLeft="400px";
-          elementCheckBox.setAttribute("name",uid);
+          // alert("data now is in process");
+          // alert("data is: "+data[key]);
+          if(key.substring(0,3).localeCompare("own")==0)
+          break;
+          if(key.substring(0,3).localeCompare("uid")==0)
+          uid=data[key];
+          if(key.substring(0,4).localeCompare("name")==0)
+          name=data[key];
+          if(key.substring(0,5).localeCompare("email")==0)
+          {
+            email=data[key];
+            element=document.createElement("div");
+            element.setAttribute("class","panel panel-primary");
+            elementHeading=document.createElement("div");
+            elementHeading.setAttribute("class","panel-heading");
+            elementBody=document.createElement("div");
+            elementBody.setAttribute("class","panel-body");
+            elementNode=document.createTextNode(name);
+            elementBodyNode=document.createTextNode(email);
+            elementCheckBox=document.createElement("input");
+            elementCheckBox.setAttribute("type","checkbox");
+            elementCheckBox.style.marginLeft="400px";
+            elementCheckBox.setAttribute("name",uid);
 
-          //now the linking
+            //now the linking
 
-          elementBody.appendChild(elementBodyNode);
-          elementBody.appendChild(elementCheckBox);
+            elementBody.appendChild(elementBodyNode);
+            elementBody.appendChild(elementCheckBox);
 
-          elementHeading.appendChild(elementNode);
-          element.appendChild(elementHeading);
-          element.appendChild(elementBody);
+            elementHeading.appendChild(elementNode);
+            element.appendChild(elementHeading);
+            element.appendChild(elementBody);
 
-          document.getElementById("wellContent").appendChild(element);
+            document.getElementById("wellContent").appendChild(element);
+          }
         }
       }
-    }
-    if(ownTours!=0)
-    {
-      /*
-      <div class="row">
-        <div class="col-sm-6 col-md-4">
-          <!-- <div class="thumbnail"> -->
-            <img src="images/image3.jpg" alt="tour" style="width:175px;height:150px;">
-            <!-- <div class="caption"> -->
-              <h3>Thumbnail label</h3>
-              <p>...</p>
-              <p><a href="#" class="btn btn-primary" role="button">Button</a></p>
-            <!-- </div> -->
-          <!-- </div> -->
-        </div>
-      </div>
-
-      this is the structure that we need to build
-
-      */
-      var element,elementNode;
-      for (var key in data)
+      if(ownTours!=0)
       {
-        
+        var element,elementNode;
+        element=document.createElement("div");
+        element.setAttribute("class","alert alert-info");
+        elementNode=document.createTextNode("My Tours");
+        element.appendChild(elementNode);
+        document.getElementById("tourDiv").appendChild(element);
+
+        var elementRow,elementCol,elementImage,heading,headingContent,inner,innerContent,buttonElement,buttonNode;
+        var dest,dateJ,dateR,description;
+        var i=0;
+        for (var key in data)
+        {
+          if(i%2==0)
+          {
+            elementRow=document.createElement("div");
+            elementRow.setAttribute("class","row");
+            document.getElementById("tourDiv").appendChild(elementRow);
+          }
+          if(key.substring(0,4).localeCompare("dest")==0)
+          {
+            dest=data[key];
+
+          }
+          if(key.substring(0,7).localeCompare("dateOfJ")==0)
+          {
+            dateJ=data[key];
+            dateJ=dateJ.substring(0,10);
+          }
+          if(key.substring(0,7).localeCompare("dateOfR")==0)
+          {
+            dateR=data[key];
+            dateR=dateR.substring(0,10);
+          }
+          if(key.substring(0,4).localeCompare("desc")==0)
+          {
+            i++;
+            description=data[key];
+            elementCol=document.createElement("div");
+            elementCol.setAttribute("class","col-sm-6 col-sm-6");
+            elementImage=document.createElement("img");
+            elementImage.setAttribute("src","images/image3.jpg");
+            elementImage.style.width="250px";
+            elementImage.style.width="300px";
+            heading=document.createElement("h3");
+            headingContent=document.createTextNode(dest);
+            inner=document.createElement("p");
+            innerContent=document.createTextNode("journey:"+dateJ+"; \n return:"+dateR+"\n "+description);
+            buttonElement=document.createElement("a");
+            buttonElement.setAttribute("class","btn btn-primary");
+            buttonElement.setAttribute("role","button");
+            buttonNode=document.createTextNode("Check out");
+
+            //now the linking
+
+            buttonElement.appendChild(buttonNode);
+            heading.appendChild(headingContent);
+            inner.appendChild(innerContent);
+
+            elementCol.appendChild(elementImage);
+            elementCol.appendChild(heading);
+            elementCol.appendChild(inner);
+            elementCol.appendChild(buttonElement);
+
+            elementRow.appendChild(elementCol);
+          }
+
+        }
+
+      }
+      if(invites==0&&ownTours==0)
+      {
+
       }
 
-    }
-    if(invites==0&&ownTours==0)
-    {
+  },1000);//1000 miliseconds later page loads
 
-    }
+
 });
 
 };
